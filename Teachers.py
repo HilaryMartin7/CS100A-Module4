b50c4d969c2b4b84df5277544d62549fbafa9e38
Martin, Hilary T <hilary.t.martin@intel.com>
2024-10-21 20:52:53 -0700

    Teachers Updated

---
+++ Teachers.py
@@ -0,0 +1,38 @@
+class Facilty:
+    def __init__(self):
+        """
+        Create a default faculty member
+        """
+
+        self.name = ''
+        self.ID = -1
+        self.hire = '1/1/2000'
+        self.department = ''
+
+    def grade(self):
+        """
+        Set time for grading
+        """
+        print(f'{self.name} is studying')
+
+    def submitgrade(self, course):
+        """
+        Submitting grades for a course
+        Parameters: course(str) the course to submit grades for
+        """
+        print(f'{self.name} grades are due for {course} course.')
+
+# Initialize a facilty object
+fac = Facilty()
+
+# Change the facilty properties
+fac.name = 'Bob'
+fac.ID = 45010
+fac.hire = '1/1/1999'
+
+print(f'#{fac.ID}: {fac.name}')
+
+fac.grade()
+fac.submitgrade('MA110')
+
+
